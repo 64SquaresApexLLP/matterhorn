dba4114b168225922104c6819c1f5fef2d192ddc
adeshgrv-innosqrs <adesh.gaur@innosquares.com>
2025-07-01 10:03:32 +0530

    minor height change in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -77,7 +77,7 @@ function HomePage() {
             </div>
 
             {/* Hero Section */}
-            <section className="relative flex flex-col items-center justify-center px-4 md:px-8 pt-24 pb-12 max-w-7xl mx-auto text-center min-h-[60vh] overflow-hidden">
+            <section className="relative flex flex-col items-center justify-center px-4 md:px-8 pt-24 pb-12 max-w-7xl mx-auto text-center min-h-screen overflow-hidden">
 
                 {/* Background Images - Positioned Left & Right */}
                 <div className="absolute inset-0 -z-10">
